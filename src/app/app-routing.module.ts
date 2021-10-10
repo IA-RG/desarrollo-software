af8915aefb2b3598883c115e30dd30b40abf927e
Alan Rodríguez <Alan Rodrguez>
2021-10-09 21:08:30 -0500

    añadido router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscadorComponent } from './buscador/buscador.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'busqueda',
+  component: BuscadorComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/classes/Formulario.ts
+++ src/app/shared/classes/Formulario.ts
@@ -6,38 +6,31 @@ export class Formulario {
   private _controladorDeFormulario: FormGroup;
   private _formulario: Pregunta<string | boolean>[];
   private _titulo: string;
-  private _grupoDeCheckbox: Pregunta<string | boolean>[]=[];
+  private _grupoDeCheckbox: Pregunta<string | boolean>[] = [];
 
   constructor(formulario: Pregunta<string | boolean>[], titulo: string) {
     this._formulario = formulario;
     this._controladorDeFormulario =
       this.asociarFormularioAControlador(formulario);
     this._titulo = titulo;
-    
   }
 
-  public obtenerGrupo()
-  {
-    
-    this._formulario.forEach((pregunta)=>{
-      console.log(pregunta);
-      if(pregunta.controlType=="checkbox")
-      {
-        this._grupoDeCheckbox.push(pregunta); 
+  public obtenerGrupo() {
+    this._formulario.forEach((pregunta) => {
+      if (pregunta.controlType == 'checkbox') {
+        this._grupoDeCheckbox.push(pregunta);
       }
     });
-    const contieneCheckbox= (pregunta:Pregunta<string| boolean>)=>pregunta.controlType ==="checkbox";
-    while(this._formulario.some(contieneCheckbox))
-    {
-      this._formulario.forEach((pregunta)=>{
-        if(pregunta.controlType=="checkbox")
-      { 
-        const index=this._formulario.indexOf(pregunta);
-        this._formulario.splice(index,1);
-      }
+    const contieneCheckbox = (pregunta: Pregunta<string | boolean>) =>
+      pregunta.controlType === 'checkbox';
+    while (this._formulario.some(contieneCheckbox)) {
+      this._formulario.forEach((pregunta) => {
+        if (pregunta.controlType == 'checkbox') {
+          const index = this._formulario.indexOf(pregunta);
+          this._formulario.splice(index, 1);
+        }
       });
     }
-    
   }
 
   private asociarFormularioAControlador(
@@ -57,7 +50,7 @@ export class Formulario {
     return this._controladorDeFormulario.getRawValue();
   }
 
-  public get grupoDeCheckbox() : Pregunta<string | boolean>[] {
+  public get grupoDeCheckbox(): Pregunta<string | boolean>[] {
     return this._grupoDeCheckbox;
   }
   public get titulo(): string {
