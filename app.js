9a51a167cd50e5a1e15abc932a6d364b92c139ab
Alan Rodríguez <Alan Rodrguez>
2021-11-15 23:03:56 -0600

    busqueda añadida

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const mysql = require("mysql2");
 const express = require("express");
 const cors = require("cors");
-
-
+const env= require("./functions/envvar");
+const tesisFunctions = require("./functions/tesis");
 const consultas = require("./functions/consultas2");
+
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: env.password(),
   database: "proyectotesis",
 });
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: env.password(),
+  database: "proyectotesis",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const promisePool = pool.promise();
 
 const app = express();
 
@@ -72,7 +85,7 @@ app.get("/profesor/:profe", (req, res) => {
   );
 });
 
-app.get("/busqueda/:busqueda", (req, res) => {
+app.get("/busqueda/:busqueda", async (req, res) => {
   const formData = JSON.parse(req.params.busqueda);
   console.log(formData);
   const consulta = consultas.obtenerConsulta(
@@ -87,94 +100,13 @@ app.get("/busqueda/:busqueda", (req, res) => {
     formData.year,
     formData.consulta
   );
-  console.log(consulta);
-
-  connection.query({sql:consulta, rowsAsArray: true},  (err, result, fields) => {
-    if (err) {
-      console.log(err);
-      res.status(404).send("Error");
-    } else {
-      console.log(result);
-      // console.log(fields);
-      const tesis = [];
-      // if(result typeof )
-      result.forEach((row) => {
-        const id = row.id;
-        const obtenerAutores = `select a.fk_tesis as id,concat(a.nombre,' ',a.apellidoPaterno,' ',a.apellidoMaterno) as nombre from tesis t join autor a on t.id=a.fk_tesis where a.fk_tesis=${id};`;
-        const obtenerSinodales = `select d.fk_tesis as id, concat(p.nombre,' ',p.apellidoPaterno,' ',p.apellidoMaterno) from tesis t join sinodal_tesis d on t.id=d.fk_tesis join profesor p on p.id=d.fk_sinodal where d.fk_tesis=${id};`;
-        const obtenerDirectores = `select d.fk_tesis as id, concat(p.nombre,' ',p.apellidoPaterno,' ',p.apellidoMaterno) from tesis t join director_tesis d on t.id=d.fk_tesis join profesor p on p.id=d.fk_director where d.fk_tesis=${id};`;
-        const obtenerPalabras = `select p.fk_tesis as id, p.fk_palabra as palabra from tesis t join palabras_tesis p on t.id=p.fk_tesis where p.fk_tesis=${id};`;
-        const obtenerTesis = `select id,enlaceAPdf,titulo,carrera,grado,year1,resumen from tesis where id=${id};`;
-        let autores = [];
-        let directores = [];
-        let sinodales = [];
-        let palabrasClave = [];
-        let datosTesis;
-        console.log(id);
-        connection.query({sql:obtenerAutores, rowsAsArray: true }, (err, result, fields) => {
-          
-          if (err) {
-            console.log(err);
-            res.status(404).send("Error obteniendo los datos");
-          } else {
-            console.log(result);
-            result.forEach((row) => {
-              autores.push(row.nombre);
-            });
-          }
-        });
-        console.log(autores);
-        connection.query({sql:obtenerDirectores, rowsAsArray: true }, (err, result, fields) => {
-          
-          if (err) {
-            console.log(err);
-            res.status(404).send("Error obteniendo los datos");
-          } else {
-            
-            result.forEach((row) => {
-              directores.push(row.nombre);
-            });
-          }
-        });
-        console.log(directores);
-        connection.query({sql:obtenerSinodales, rowsAsArray: true }, (err, result, fields) => {
-          if (err) {
-
-            console.log(err);
-            res.status(404).send("Error obteniendo los datos");
-          } else {
-            result.forEach((row) => {
-              sinodales.push(row.nombre);
-            });
-          }
-        });
-        console.log(sinodales);
-        connection.query({sql:obtenerTesis, rowsAsArray: true }, (err, result, fields) => {
-          if (err) {
-            console.log(err);
-            res.status(404).send("Error obteniendo los datos");
-          } else {
-            console.log(result);
-            datosTesis = result;
-          }
-        });
-        const tes={
-          titulo:datosTesis.titulo,
-          enlaceAPdf:datosTesis.enlaceAPdf,
-          carrera:datosTesis.carrera,
-          grado:datosTesis.grado,
-          year:datosTesis.year1,
-          resumen:datosTesis.resumen,
-          autores:autores,
-          directores:directores,
-          sinodales:sinodales,
-          palabrasClave:palabrasClave
-        };
-        tesis.push(tes);
-      });
-      res.status(200).send(JSON.stringify(tesis));
-    }
+  // console.log(consulta);
+  tesisFunctions.obtenerTesis(consulta,promisePool,res).then((tesisResultantes)=>{
+    console.log(tesisResultantes);
+    res.status(200).send(JSON.stringify({tesis:tesisResultantes}));
   });
+  // console.log(tesisResultantes);
+  
 });
 
 app.post("/tesis", (req, res)=>{
---
+++ functions/tesis.js
@@ -0,0 +1,146 @@
+async function obtenerTesis(consulta, promisePool, res) {
+  const [ids, fields] = await promisePool
+    .query(consulta)
+    .catch((err) => res.status(404).send("Error. Intente de nuevo"));
+  // console.log(ids);
+
+  return await obtenerArregloDeTesis(promisePool, res, ids);
+}
+
+async function obtenerArregloDeTesis(promisePool, res, ids) {
+  const tesisResultantes = [];
+  var obtenerAutores;
+  var obtenerSinodales;
+  var obtenerDirectores;
+  var obtenerPalabras;
+  var obtenerTesis;
+  await Promise.all(ids.map(async (id) => {
+    obtenerAutores = `select a.fk_tesis as id,concat(a.nombre,' ',a.apellidoPaterno,' ',a.apellidoMaterno) as nombre from tesis t join autor a on t.id=a.fk_tesis where a.fk_tesis=${id.id};`;
+    obtenerSinodales = `select d.fk_tesis as id, concat(p.nombre,' ',p.apellidoPaterno,' ',p.apellidoMaterno) as nombre from tesis t join sinodal_tesis d on t.id=d.fk_tesis join profesor p on p.id=d.fk_sinodal where d.fk_tesis=${id.id};`;
+    obtenerDirectores = `select d.fk_tesis as id, concat(p.nombre,' ',p.apellidoPaterno,' ',p.apellidoMaterno) as nombre from tesis t join director_tesis d on t.id=d.fk_tesis join profesor p on p.id=d.fk_director where d.fk_tesis=${id.id};`;
+    obtenerPalabras = `select p.fk_tesis as id, p.fk_palabra as palabra from tesis t join palabras_tesis p on t.id=p.fk_tesis where p.fk_tesis=${id.id};`;
+    obtenerTesis = `select id,enlaceAPdf,titulo,carrera,grado,year1,resumen from tesis where id=${id.id};`;
+    const [autoresFilas, fieldsa] = await promisePool
+      .query(obtenerAutores)
+      .catch((err) => res.status(404).send("Error. Intente de nuevo"));
+    const [sinodalesFilas, fieldss] = await promisePool
+      .query(obtenerSinodales)
+      .catch((err) => res.status(404).send("Error. Intente de nuevo"));
+    const [directoresFilas, fieldsd] = await promisePool
+      .query(obtenerDirectores)
+      .catch((err) => res.status(404).send("Error. Intente de nuevo"));
+    const [palabrasFilas, fieldsp] = await promisePool
+      .query(obtenerPalabras)
+      .catch((err) => res.status(404).send("Error. Intente de nuevo"));
+    const [tesisFila, fieldst] = await promisePool
+      .query(obtenerTesis)
+      .catch((err) => res.status(404).send("Error. Intente de nuevo"));
+
+    let autores = [];
+    let sinodales = [];
+    let directores = [];
+    let palabras = [];
+    if (tesisFila.length > 0) {
+      // console.log(autoresFilas);
+      autoresFilas.forEach((autorFila) => {
+        autores.push(autorFila.nombre);
+      });
+
+      // console.log(sinodalesFilas);
+      sinodalesFilas.forEach((sinodalFila) => {
+        sinodales.push(sinodalFila.nombre);
+      });
+
+      // console.log(directoresFilas);
+      directoresFilas.forEach((directorFila) => {
+        directores.push(directorFila.nombre);
+      });
+
+      // console.log(palabrasFilas);
+      palabrasFilas.forEach((palabraFila) => {
+        palabras.push(palabraFila.palabra);
+      });
+
+      // console.log(tesisFila);
+      tesisResultantes.push({
+        enlaceAPdf: tesisFila[0].enlaceAPdf,
+        titulo: tesisFila[0].titulo,
+        carrera: tesisFila[0].carrera,
+        grado: tesisFila[0].grado,
+        year: tesisFila[0].year1,
+        resumen: tesisFila[0].resumen,
+        autores: autores,
+        sinodales: sinodales,
+        directores: directores,
+        palabrasClave: palabras,
+      });
+    }
+  }));
+  return tesisResultantes;
+}
+
+module.exports.obtenerTesis = obtenerTesis;
+
+
+// ids.forEach( async (id) => {
+  //   obtenerAutores = `select a.fk_tesis as id,concat(a.nombre,' ',a.apellidoPaterno,' ',a.apellidoMaterno) as nombre from tesis t join autor a on t.id=a.fk_tesis where a.fk_tesis=${id.id};`;
+  //   obtenerSinodales = `select d.fk_tesis as id, concat(p.nombre,' ',p.apellidoPaterno,' ',p.apellidoMaterno) as nombre from tesis t join sinodal_tesis d on t.id=d.fk_tesis join profesor p on p.id=d.fk_sinodal where d.fk_tesis=${id.id};`;
+  //   obtenerDirectores = `select d.fk_tesis as id, concat(p.nombre,' ',p.apellidoPaterno,' ',p.apellidoMaterno) as nombre from tesis t join director_tesis d on t.id=d.fk_tesis join profesor p on p.id=d.fk_director where d.fk_tesis=${id.id};`;
+  //   obtenerPalabras = `select p.fk_tesis as id, p.fk_palabra as palabra from tesis t join palabras_tesis p on t.id=p.fk_tesis where p.fk_tesis=${id.id};`;
+  //   obtenerTesis = `select id,enlaceAPdf,titulo,carrera,grado,year1,resumen from tesis where id=${id.id};`;
+  //   const [autoresFilas, fieldsa] = await promisePool
+  //     .query(obtenerAutores)
+  //     .catch((err) => res.status(404).send("Error. Intente de nuevo"));
+  //   const [sinodalesFilas, fieldss] = await promisePool
+  //     .query(obtenerSinodales)
+  //     .catch((err) => res.status(404).send("Error. Intente de nuevo"));
+  //   const [directoresFilas, fieldsd] = await promisePool
+  //     .query(obtenerDirectores)
+  //     .catch((err) => res.status(404).send("Error. Intente de nuevo"));
+  //   const [palabrasFilas, fieldsp] = await promisePool
+  //     .query(obtenerPalabras)
+  //     .catch((err) => res.status(404).send("Error. Intente de nuevo"));
+  //   const [tesisFila, fieldst] = await promisePool
+  //     .query(obtenerTesis)
+  //     .catch((err) => res.status(404).send("Error. Intente de nuevo"));
+
+  //   let autores = [];
+  //   let sinodales = [];
+  //   let directores = [];
+  //   let palabras = [];
+  //   if (tesisFila.length > 0) {
+  //     // console.log(autoresFilas);
+  //     autoresFilas.forEach((autorFila) => {
+  //       autores.push(autorFila.nombre);
+  //     });
+
+  //     // console.log(sinodalesFilas);
+  //     sinodalesFilas.forEach((sinodalFila) => {
+  //       autores.push(sinodalFila.nombre);
+  //     });
+
+  //     // console.log(directoresFilas);
+  //     directoresFilas.forEach((directorFila) => {
+  //       directores.push(directorFila.nombre);
+  //     });
+
+  //     // console.log(palabrasFilas);
+  //     palabrasFilas.forEach((palabraFila) => {
+  //       palabras.push(palabraFila.palabra);
+  //     });
+
+  //     // console.log(tesisFila);
+  //     tesisResultantes.push({
+  //       enlaceAPdf: tesisFila[0].enlaceAPdf,
+  //       titulo: tesisFila[0].titulo,
+  //       carrera: tesisFila[0].carrera,
+  //       grado: tesisFila[0].grado,
+  //       year: tesisFila[0].year1,
+  //       resumen: tesisFila[0].resumen,
+  //       autores: autores,
+  //       sinodales: sinodales,
+  //       directores: directores,
+  //       palabrasClave: palabras,
+  //     });
+  //   }
+  // });
